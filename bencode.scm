8f037b42ef3672f1b0e1620f0f50f2efacfdf17a
Caolan McMahon <caolan@caolanmcmahon.com>
2015-11-06 16:21:04 +0000

    first commit

---
+++ bencode.scm
@@ -0,0 +1,160 @@
+(module bencode (read-bencode write-bencode)
+
+(import chicken scheme)
+(use data-structures extras miscmacros ports vector-lib)
+
+(define discard-char read-char)
+
+(define (digit? ch)
+  (and (char>=? ch #\0) (char<=? ch #\9)))
+
+(define (unexpected-end position)
+  (error 'read-bencode
+         (sprintf "unexpected end of input at char ~A"
+           position)))
+
+(define (unexpected-char ch position)
+  (error 'read-bencode
+         (sprintf "unexpected character '~A' at char ~A"
+           ch
+           position)))
+
+(define (make-decode-fold iterator initial finally)
+  (lambda (start)
+    (discard-char) ;; beginning marker
+    (let loop ((ch (peek-char))
+               (acc initial)
+               (position (+ start 1)))
+      (cond
+       ((char=? ch #\e)
+        (discard-char) ;; discard end marker #\e
+        (values (finally acc) (+ position 1)))
+       ((eof-object? ch)
+        (unexpected-end (+ position 1)))
+       (else
+        (receive (data new-position) (decode position)
+          (loop (peek-char)
+                (iterator data (+ position 1) new-position acc)
+                new-position)))))))
+
+(define (decode-int* delimiter negative start)
+  (let loop ((chars '())
+             (position (+ start 1))
+             (ch (read-char)))
+    (cond
+     ((char=? ch delimiter)
+      (let ((n (string->number (list->string (reverse chars)))))
+        (if n
+            (values n position)
+            (unexpected-char ch position))))
+     ((eof-object? ch)
+      (unexpected-end position))
+     ((or (digit? ch) (and negative (char=? ch #\-)))
+      (loop (cons ch chars)
+            (+ position 1)
+            (read-char)))
+     (else
+      (unexpected-char ch position)))))
+
+(define (decode-string position)
+  (receive (len position) (decode-int* #\: #f position)
+    (let ((str (make-string len)))
+      (if (< (read-string! len str) len)
+          (unexpected-end (+ position len))
+          (values str (+ position len))))))
+
+(define (decode-integer position)
+  (discard-char) ;; discard #\i
+  (decode-int* #\e #t (+ position 1)))
+
+(define decode-list
+  (make-decode-fold
+   (lambda (x start end acc)
+     (cons x acc))
+   '()
+   (compose list->vector reverse)))
+
+(define (decode-dictionary position)
+  (let ((key #f) (last-key #f))
+    ((make-decode-fold
+      (lambda (x start end acc)
+        (if key
+            (begin0
+             (cons (cons (string->symbol key) x) acc)
+             (set! last-key key)
+             (set! key #f))
+            (begin
+              (unless (string? x)
+                (error 'read-bencode
+                       (sprintf "expected bencoded string as key at char ~A"
+                         start)))
+              (when (and last-key (string<? x last-key))
+                (error 'read-bencode
+                  (sprintf
+                    "key ~S starting at char ~A is not in lexographical order"
+                    x
+                    start)))
+              (set! key x)
+              acc)))
+      '()
+      reverse)
+     position)))
+
+(define (decode #!optional (position 0))
+  (let ((ch (peek-char)))
+    (cond
+     ((digit? ch) (decode-string position))
+     ((char=? ch #\i) (decode-integer position))
+     ((char=? ch #\l) (decode-list position))
+     ((char=? ch #\d) (decode-dictionary position))
+     (else
+      (unexpected-char ch (+ position 1))))))
+
+(define (read-bencode #!optional (port (current-input-port)))
+  (with-input-from-port port
+    (lambda ()
+      (receive (data position) (decode)
+        data))))
+
+(define (invalid-type x)
+  (error 'write-bencode
+         "can only encode alist, vector, integer, or string"))
+
+(define (encode-string x)
+  (display (conc (string-length x) ":" x)))
+
+(define (encode-integer x)
+  (display (conc "i" x "e")))
+
+(define (encode-list x)
+  (display "l")
+  (vector-for-each (lambda (i y) (encode y)) x)
+  (display "e"))
+
+(define (encode-dictionary x)
+  (display "d")
+  (for-each
+   (lambda (y)
+     (encode (car y))
+     (encode (cdr y)))
+   (sort
+    (map (lambda (p)
+           (unless (pair? p) (invalid-type p))
+           (unless (symbol? (car p)) (invalid-type (car p)))
+           (cons (symbol->string (car p)) (cdr p)))
+         x)
+    (lambda (a b)
+      (string<? (car a) (car b)))))
+  (display "e"))
+
+(define (encode x)
+  (cond ((string? x) (encode-string x))
+        ((integer? x) (encode-integer x))
+        ((vector? x) (encode-list x))
+        ((or (pair? x) (null? x)) (encode-dictionary x))
+        (else (invalid-type x))))
+
+(define (write-bencode data #!optional (port (current-output-port)))
+  (with-output-to-port port (cut encode data)))
+
+)
---
+++ tests/run.scm
@@ -0,0 +1,177 @@
+(use test bencode)
+
+(define-syntax decode-test
+  (syntax-rules ()
+    ((_ description expected input)
+     (test description
+           expected
+           (call-with-input-string input read-bencode)))))
+
+(define-syntax decode-error-test
+  (syntax-rules ()
+    ((_ description expected input)
+     (test description
+           expected
+           (handle-exceptions exn
+             (get-condition-property exn 'exn 'message "no message supplied")
+             (call-with-input-string input read-bencode))))))
+
+(test-group "read-bencode: strings"
+  (decode-test "basic string" "spam" "4:spam")
+  (decode-test "empty string" "" "0:")
+  (decode-test "string including ':' char" "foo:bar" "7:foo:bar")
+  (decode-test "string including null char"
+               (list->string '(#\f #\o #\o #\null))
+               (list->string '(#\4 #\: #\f #\o #\o #\null)))
+  (decode-error-test "string too short"
+                     "unexpected end of input at char 6"
+                     "4:foo")
+  (decode-error-test "non digit in length prefix"
+                     "unexpected character 'x' at char 2"
+                     "1x2:hello world!")
+  (decode-error-test "negative prefix"
+                     "unexpected character '-' at char 1"
+                     "-12:hello world!"))
+
+(test-group "read-bencode: integers"
+  (decode-test "integer" 42 "i42e")
+  (decode-test "negative integer" -42 "i-42e")
+  (decode-test "zero" 0 "i0e")
+  (decode-error-test "empty integer"
+                     "unexpected character 'e' at char 2"
+                     "ie"))
+
+(test-group "read-bencode: lists"
+  (decode-test "list with strings and integers"
+               #("hello" 42)
+               "l5:helloi42ee")
+  (decode-test "empty list"
+               #()
+               "le")
+  (decode-test "nested lists"
+               #("hello" 42 #(-1 0 1 2 3 "four") "five")
+               "l5:helloi42eli-1ei0ei1ei2ei3e4:foure4:fivee")
+  (decode-test "multiple empty lists"
+               #(#() #() #(#(#(#()))))
+               "llelelllleeeee")
+  (decode-error-test "unclosed list segment"
+                     "unexpected end of input at char 8"
+                     "lllleee"))
+
+(test-group "read-bencode: dictionaries"
+  (decode-test "dictionary with strings and integers"
+               '((age . 100) (name . "the dude"))
+               "d3:agei100e4:name8:the dudee")
+  (decode-test "nested dictionary"
+               '((age . 100)
+                 (info . ((email . "dude@dude.com") (number . 2488081446)))
+                 (name . "the dude"))
+               (string-append
+                "d3:agei100e"
+                "4:infod5:email13:dude@dude.com6:numberi2488081446ee"
+                "4:name8:the dudee"))
+  (decode-test "empty dictionary"
+               '()
+               "de")
+  (decode-error-test "non-sorted dictionaries"
+                     "key \"aaa\" starting at char 12 is not in lexographical order"
+                     "d3:zzz3:bar3:aaai456ee")
+  (decode-error-test "non-string keys should error"
+                     "expected bencoded string as key at char 12"
+                     "d3:aaa3:bari123ei456ee")
+  (decode-error-test "unclosed dictionary segment"
+                     "unexpected end of input at char 21"
+                     "d3:aaad3:bbbd3:cccee"))
+
+(test-group "read-bencode: mixed data from current-port"
+  (test "dictionaries, lists, integers and strings"
+        '((extra . #(#(((favorites . #()))) 500))
+          (locations . #(((address . "484 street"))
+                         ((address . "828 street")))))
+        (with-input-from-string
+            (string-append
+             "d5:extralld9:favoritesleeei500ee"
+             "9:locationsld7:address10:484 streeted"
+             "7:address10:828 streeteee")
+          read-bencode)))
+
+(define-syntax encode-test
+  (syntax-rules ()
+    ((_ description expected input)
+     (test description
+           expected
+           (call-with-output-string (cut write-bencode input <>))))))
+
+(define-syntax encode-error-test
+  (syntax-rules ()
+    ((_ description expected input)
+     (test description
+           expected
+           (handle-exceptions exn
+             (get-condition-property exn 'exn 'message "no message supplied")
+             (call-with-output-string (cut write-bencode input <>)))))))
+
+(test-group "write-bencode: strings"
+  (encode-test "basic string"
+               "13:Hello, world!"
+               "Hello, world!")
+  (encode-test "empty string"
+               "0:"
+               ""))
+
+(test-group "write-bencode: integers"
+  (encode-test "integer" "i42e" 42)
+  (encode-test "negative integer" "i-42e" -42)
+  (encode-test "zero" "i0e" 0))
+
+(test-group "write-bencode: lists"
+  (encode-test "list with strings and integers"
+               "l5:helloi42ee"
+               #("hello" 42))
+  (encode-test "empty list"
+               "le"
+               #())
+  (encode-test "nested lists"
+               "l5:helloi42eli-1ei0ei1ei2ei3e4:foure4:fivee"
+               #("hello" 42 #(-1 0 1 2 3 "four") "five"))
+  (encode-test "multiple empty lists"
+               "llelelllleeeee"
+               #(#() #() #(#(#(#()))))))
+
+(test-group "write-bencode: dictionaries"
+  (encode-test "dictionary with strings and integers"
+               "d3:agei100e4:name8:the dudee"
+               '((age . 100) (name . "the dude")))
+  (encode-test "nested dictionary"
+               (string-append
+                "d3:agei100e"
+                "4:infod5:email13:dude@dude.com6:numberi2488081446ee"
+                "4:name8:the dudee")
+               '((age . 100)
+                 (info . ((email . "dude@dude.com") (number . 2488081446)))
+                 (name . "the dude")))
+  (encode-test "empty dictionary"
+               "de"
+               '())
+  (encode-test "sort dictionary keys"
+               "d7:integeri12345e6:string13:Hello, world!e"
+               '((string . "Hello, world!")
+                 (integer . 12345))))
+
+(define-record testrecord x)
+(define-record-printer (testrecord r port)
+  (fprintf "<testrecord:~S>" (testrecord-x r)))
+
+(test-group "write-bencode: invalid types"
+  (encode-error-test "non-alist list"
+                     "can only encode alist, vector, integer, or string"
+                     '(1 2 3 4))
+  (encode-error-test "non-integer number"
+                     "can only encode alist, vector, integer, or string"
+                     123.456)
+  (encode-error-test "record"
+                     "can only encode alist, vector, integer, or string"
+                     (make-testrecord "foo"))
+  (encode-error-test "symbol"
+                     "can only encode alist, vector, integer, or string"
+                     'asdf))
